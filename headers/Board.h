6344b1bf05ed07db038de912715c08929c597fb0
Adam Borenstein <adam200il@gmail.com>
2022-08-31 21:36:08 +0300

    created h file

---
+++ headers/Board.h
@@ -0,0 +1,13 @@
+#if !defined(BOARD.h)
+#define BOARD.h
+
+namespace AdamBorenstein
+{
+class Board
+{
+    Board( int width = 7, int length = 7 );
+} // end of class board
+} // end of namespace 
+
+
+#endif // BOARD.h
