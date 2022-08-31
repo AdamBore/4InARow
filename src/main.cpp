50283bd635447f99f6e9f49e6b267dc3a19f73dc
Adam Borenstein <adam200il@gmail.com>
2022-08-31 21:26:23 +0300

    printing greeating

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std; 
+
+void PrintGreatting(); 
+
+int main(int argc, char const *argv[])
+{
+	PrintGreatting();
+	return 0;
+}
+
+void PrintGreatting () {
+	cout << "Wellcome to 4 in a row game" << endl << "Please select solid or blank" << endl;
+}
