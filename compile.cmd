be2881bbb60e4b873ecb6baa1fff0b61f11e79e1
Adam Borenstein <adam200il@gmail.com>
2022-09-01 10:29:44 +0300

    added Werror Wall pedantic and changed include lib

--- compile.cmd
+++ compile.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-g++ -o E:\Code\c++\4InARow\bin\play.exe .\src\main.cpp
+g++ -pedantic -Werror -Wall -o E:\Code\c++\4InARow\bin\play.exe .\src\main.cpp .\src\cpps\Board.cpp -I E:\Code\c++\4InARow\src\headers\
