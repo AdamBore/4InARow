7843cfa0b4d502e029147e0010a856dcde229e51
Adam Borenstein <adam200il@gmail.com>
2022-09-01 10:53:48 +0300

    added dtor

--- src/cpps/Board.cpp
+++ src/cpps/Board.cpp
@@ -7,4 +7,8 @@ Board::Board( int width, int length ){
     gameBoard = new int[ ( width + 1 ) * ( length + 1 )]; // + 1 to allow for headers to be added
 }
 
+Board::~Board(){
+    delete gameBoard; 
+}
+
 }
