42504159997c1309cb5a1a335af7358718f54efb
Adam Borenstein <adam200il@gmail.com>
2022-09-01 10:31:40 +0300

    added constractor with default parameters

---
+++ src/cpps/Board.cpp
@@ -0,0 +1,10 @@
+#include "Board.h"
+
+namespace AdamBorenstein
+{
+
+Board::Board( int width, int length ){
+    gameBoard = new int[ ( width + 1 ) * ( length + 1 )]; // + 1 to allow for headers to be added
+}
+
+}
