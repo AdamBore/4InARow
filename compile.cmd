6f6b6c19ab541239b074e687c340670149bf28f5
Adam Borenstein <adam200il@gmail.com>
2022-09-01 10:53:30 +0300

    added game.cpp to compile

--- compile.cmd
+++ compile.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-g++ -pedantic -Werror -Wall -o E:\Code\c++\4InARow\bin\play.exe .\src\main.cpp .\src\cpps\Board.cpp -I E:\Code\c++\4InARow\src\headers\
+g++ -pedantic -Werror -Wall -o E:\Code\c++\4InARow\bin\play.exe .\src\main.cpp .\src\cpps\Board.cpp  .\src\cpps\Game.cpp -I E:\Code\c++\4InARow\src\headers\
